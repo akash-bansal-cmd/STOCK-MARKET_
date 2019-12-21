7d3c0a0acc36e8ee4991945d127355f37526c34c
akash-bansal-cmd <11akashbansal@gmail.com>
2019-12-21 12:08:15 +0530

    Stockmarket

---
+++ StockMarket/src/app/service/user-auth-guard.service.ts
@@ -0,0 +1,17 @@
+import { Injectable } from '@angular/core';
+import { Router } from '@angular/router';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class UserAuthGuardService {
+
+  constructor(private router:Router) { }
+
+  canActivateChild() {
+    if(localStorage.getItem('role') === 'ROLE_USER') {
+      return true;
+    }
+   this.router.navigate(['login'])
+  }
+}
